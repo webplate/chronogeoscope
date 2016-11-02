d96681e577974c5d55006d2d26aec9967dd43625
Glen <glen@mailoo.org>
2016-11-02 23:26:34 +0100

    i18n

--- server.py
+++ server.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+
 # std lib
 import os
 # modules
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request
+# i18n
+from flask_babel import Babel
 
 app = Flask(__name__)
 app.secret_key = 'ENQG8h47Lk7q546q8q4q7522qTD7Fjvjvqhjlvqjl5465465qvqv5q56v42Ac543ttZE69C3m23xVhE6'
+babel = Babel(app)
 
 app_folder = os.path.split(os.path.abspath(__file__))[0]
 
+
+LANGUAGES = {
+    'en': 'English',
+    'fr': 'French',
+    'it': 'Italian'
+}
+
+# send translation according to browser header
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(LANGUAGES.keys())
 
 @app.route('/')
 def main_page():
